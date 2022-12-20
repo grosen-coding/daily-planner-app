a7802e1dfae9ea6e3a2a7cc981dd56de46968bd2
grosen-coding <rosengsr@gmail.com>
2022-12-20 00:06:00 -0500

    finished login page styling

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { FaSignInAlt } from 'react-icons/fa'
 import { useSelector, useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { login, reset } from '../features/auth/authSlice'
 import Spinner from '../components/Spinner'
@@ -57,45 +57,52 @@ function Login() {
 
   return (
     <>
-      <section className='heading'>
-        <h1>
-          <FaSignInAlt /> Login
-        </h1>
-        <p>Login and start setting goals</p>
-      </section>
+      <div className='login-container'>
+        <section className='login-heading'>
+          <h1>
+            Welcome!!
+          </h1>
+          <p>Please login below to begin planning your day!</p>
+        </section>
 
-      <section className='form'>
-        <form onSubmit={onSubmit}>
-          <div className='form-group'>
-            <input
-              type='email'
-              className='form-control'
-              id='email'
-              name='email'
-              value={email}
-              placeholder='Enter your email'
-              onChange={onChange}
-            />
-          </div>
-          <div className='form-group'>
-            <input
-              type='password'
-              className='form-control'
-              id='password'
-              name='password'
-              value={password}
-              placeholder='Enter password'
-              onChange={onChange}
-            />
-          </div>
+        <section className='form'>
+          <form onSubmit={onSubmit}>
+            <div className='form-group'>
+              <input
+                type='email'
+                className='form-control'
+                id='email'
+                name='email'
+                value={email}
+                placeholder='Enter your email'
+                onChange={onChange}
+              />
+            </div>
+            <div className='form-group'>
+              <input
+                type='password'
+                className='form-control'
+                id='password'
+                name='password'
+                value={password}
+                placeholder='Enter password'
+                onChange={onChange}
+              />
+            </div>
 
-          <div className='form-group'>
-            <button type='submit' className='btn btn-block'>
-              Submit
-            </button>
-          </div>
-        </form>
-      </section>
+            <div className='form-group'>
+              <button type='submit' className='btn btn-block'>
+                Submit
+              </button>
+            </div>
+          </form>
+
+        </section>
+          <section className='login-register'>
+            <p>New user? Please <Link to='/register'>
+                <span>Register</span></Link> first to get started.</p>
+          </section>
+      </div>
     </>
   )
 }
