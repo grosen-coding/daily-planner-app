ea0efeb9394c91bc651593022631b08b62343769
grosen-coding <rosengsr@gmail.com>
2022-12-26 20:30:22 -0500

    reveal removal push

--- frontend/src/components/FormModal.js
+++ frontend/src/components/FormModal.js
@@ -4,7 +4,7 @@ import PriorityForm from "./PriorityForm";
 import ReminderForm from "./ReminderForm";
 import TodoForm from "./TodoForm";
 import { AiFillCloseCircle } from "react-icons/ai";
-import Bounce from "react-reveal/Bounce";
+// import Bounce from "react-reveal/Bounce";
 
 export const FormModal = ({ showModal, setShowModal }) => {
   const modalRef = useRef();
@@ -34,21 +34,21 @@ export const FormModal = ({ showModal, setShowModal }) => {
     <>
       {showModal ? (
         <div className="form-modal" onClick={closeModal} ref={modalRef}>
-          <Bounce top>
-            <div className="modal-container" showModal={showModal}>
-              <div className="modal-content">
-                <NoteForm />
-                <ReminderForm />
-                <TodoForm />
-                <PriorityForm />
-                <AiFillCloseCircle
-                  className="modal-close-icon"
-                  aria-label="Close modal"
-                  onClick={() => setShowModal((prev) => !prev)}
-                />
-              </div>
+          {/* <Bounce top> */}
+          <div className="modal-container" showModal={showModal}>
+            <div className="modal-content">
+              <NoteForm />
+              <ReminderForm />
+              <TodoForm />
+              <PriorityForm />
+              <AiFillCloseCircle
+                className="modal-close-icon"
+                aria-label="Close modal"
+                onClick={() => setShowModal((prev) => !prev)}
+              />
             </div>
-          </Bounce>
+          </div>
+          {/* </Bounce> */}
         </div>
       ) : null}
     </>
