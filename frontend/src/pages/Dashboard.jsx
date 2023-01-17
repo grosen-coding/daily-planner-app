91578834e80188efef39ed99a442eae3ecc9da24
grosen-coding <rosengsr@gmail.com>
2023-01-17 00:39:04 -0500

    Added react-reveal

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,137 +1,122 @@
-import { useEffect} from 'react'
-// import Zoom from 'react-reveal/Zoom';
-import { useNavigate } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
-import ReminderItem from '../components/ReminderItem'
-import TodoItem from '../components/TodoItem'
-import NoteItem from '../components/NoteItem'
-import PriorityItem from '../components/PriorityItem'
-import Spinner from '../components/Spinner'
-import { getGoals, reset } from '../features/goals/goalSlice'
-import { getNotes } from '../features/notes/noteSlice'
-import { getPriorities } from '../features/priorities/prioritySlice'
-import { getReminders } from '../features/reminders/reminderSlice'
-import { getTodos } from '../features/todos/todoSlice'
-
+import { useEffect } from "react";
+import Zoom from "react-reveal/Zoom";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import ReminderItem from "../components/ReminderItem";
+import TodoItem from "../components/TodoItem";
+import NoteItem from "../components/NoteItem";
+import PriorityItem from "../components/PriorityItem";
+import Spinner from "../components/Spinner";
+import { getGoals, reset } from "../features/goals/goalSlice";
+import { getNotes } from "../features/notes/noteSlice";
+import { getPriorities } from "../features/priorities/prioritySlice";
+import { getReminders } from "../features/reminders/reminderSlice";
+import { getTodos } from "../features/todos/todoSlice";
 
 function Dashboard() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-
-  const { user } = useSelector((state) => state.auth)
-  const { isLoading, isError, message } = useSelector(
-    (state) => state.goals
-  )
-  const { reminders } = useSelector(
-    (state) => state.reminders
-  )
-    const { todos } = useSelector(
-    (state) => state.todos
-  )
-  const { notes } = useSelector(
-    (state) => state.notes
-  )
-  const { priorities } = useSelector(
-    (state) => state.priorities
-  )
-
+  const { user } = useSelector((state) => state.auth);
+  const { isLoading, isError, message } = useSelector((state) => state.goals);
+  const { reminders } = useSelector((state) => state.reminders);
+  const { todos } = useSelector((state) => state.todos);
+  const { notes } = useSelector((state) => state.notes);
+  const { priorities } = useSelector((state) => state.priorities);
 
   useEffect(() => {
     if (isError) {
-      console.log(message)
+      console.log(message);
     }
 
     if (!user) {
-      navigate('/login')
+      navigate("/login");
     }
 
-    dispatch(getReminders())
-    dispatch(getTodos())
-    dispatch(getNotes())
-    dispatch(getPriorities())
-    dispatch(getGoals())
+    dispatch(getReminders());
+    dispatch(getTodos());
+    dispatch(getNotes());
+    dispatch(getPriorities());
+    dispatch(getGoals());
 
     return () => {
-      dispatch(reset())
-    }
-  }, [user, navigate, isError, message, dispatch])
+      dispatch(reset());
+    };
+  }, [user, navigate, isError, message, dispatch]);
 
   if (isLoading) {
-    return <Spinner />
+    return <Spinner />;
   }
 
-  const priorityList = priorities.length
-
+  const priorityList = priorities.length;
 
   return (
     <>
-      <section className='tasks-container'>
-{/* TOP PRIORITIES LIST */}
-      {/* <Zoom> */}
-        <div className='task-card'>
+      <section className="tasks-container">
+        {/* TOP PRIORITIES LIST */}
+        <Zoom>
+          <div className="task-card">
             <h2>Top Priorities ({priorityList})</h2>
 
             {priorities.length > 0 ? (
-              <ul className='task-items'>
+              <ul className="task-items">
                 {priorities.map((priority) => (
                   <PriorityItem key={priority._id} priority={priority} />
                 ))}
               </ul>
-          ) : (
-            <h3>You have not set any top priorities</h3>
-          )}
-
-        </div>
-      {/* </Zoom> */}
-{/* REMINDERS LIST */}
-            {/* <Zoom> */}
-        <div className='task-card'>
-          <h2>Reminders ({reminders.length})</h2>
-          {reminders.length > 0 ? (
-            <ul className='task-items'>
-              {reminders.map((reminder) => (
-                <ReminderItem key={reminder._id} reminder={reminder} />
-              ))}
-            </ul>
-          ) : (
-            <h3>You have not set any reminders</h3>
-          )}
-        </div>
-        {/* </Zoom> */}
-{/* TO DO LIST */}
-{/* <Zoom> */}
-        <div className='task-card'>
-          <h2>To-do List ({todos.length})</h2>
-          {todos.length > 0 ? (
-            <ul className='task-items'>
-              {todos.map((todo) => (
-                <TodoItem key={todo._id} todo={todo} />
-              ))}
-            </ul>
-          ) : (
-            <h3>You have not set any to-dos</h3>
-          )}
-        </div>
-        {/* </Zoom> */}
-{/* NOTES LIST */}
-{/* <Zoom> */}
-        <div className='task-card'>
-          <h2>Notes ({notes.length})</h2>
-          {notes.length > 0 ? (
-            <ul className='task-items'>
-              {notes.map((note) => (
-                <NoteItem key={note._id} note={note} />
-              ))}
-            </ul>
-          ) : (
-            <h3>You have not set any notes</h3>
-          )}
-        </div>
-        {/* </Zoom> */}
+            ) : (
+              <h3>You have not set any top priorities</h3>
+            )}
+          </div>
+        </Zoom>
+        {/* REMINDERS LIST */}
+        <Zoom>
+          <div className="task-card">
+            <h2>Reminders ({reminders.length})</h2>
+            {reminders.length > 0 ? (
+              <ul className="task-items">
+                {reminders.map((reminder) => (
+                  <ReminderItem key={reminder._id} reminder={reminder} />
+                ))}
+              </ul>
+            ) : (
+              <h3>You have not set any reminders</h3>
+            )}
+          </div>
+        </Zoom>
+        {/* TO DO LIST */}
+        <Zoom>
+          <div className="task-card">
+            <h2>To-do List ({todos.length})</h2>
+            {todos.length > 0 ? (
+              <ul className="task-items">
+                {todos.map((todo) => (
+                  <TodoItem key={todo._id} todo={todo} />
+                ))}
+              </ul>
+            ) : (
+              <h3>You have not set any to-dos</h3>
+            )}
+          </div>
+        </Zoom>
+        {/* NOTES LIST */}
+        <Zoom>
+          <div className="task-card">
+            <h2>Notes ({notes.length})</h2>
+            {notes.length > 0 ? (
+              <ul className="task-items">
+                {notes.map((note) => (
+                  <NoteItem key={note._id} note={note} />
+                ))}
+              </ul>
+            ) : (
+              <h3>You have not set any notes</h3>
+            )}
+          </div>
+        </Zoom>
       </section>
     </>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
